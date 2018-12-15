cd62fc5519bbd1e3590c79de57050fd55d7a1da1
joncody <jonjdcody@gmail.com>
2018-12-15 17:19:46 -0600

    Previous commit updates: type CookieReader added; Conn object now has a Cookie field; SocketHandler now takes one argument of type CookieReader.  Simply pass nil to wsrooms.SocketHandler to update existing code, as in the example.

--- room.go
+++ room.go
@@ -1,5 +1,5 @@
 //    Title: room.go
-//    Author: Jon Cody
+//    Author: Jonanthan David Cody
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
